7faf19cb71c613d68a745097539f007768322557
Marc R. Hoffmann <hoffmann@mountainminds.com>
2009-10-19 06:26:05 +0000

    SF SVN repository only accessible via https.

--- updatesite.sh
+++ updatesite.sh
@@ -4,9 +4,9 @@ WORKING_DIR="`pwd`/work"
 CHECKOUT_DIR=$WORKING_DIR/checkout
 RESULT_DIR=$WORKING_DIR/result
 
-SVN_URL1=http://eclemma.svn.sourceforge.net/svnroot/eclemma/eclemmasite
-SVN_URL2=http://eclemma.svn.sourceforge.net/svnroot/eclemma/eclemma/tags/v1.4.3/com.mountainminds.eclemma.doc
-SVN_URL3=http://eclemma.svn.sourceforge.net/svnroot/eclemma/eclemma/trunk/com.mountainminds.eclemma.updatesite
+SVN_URL1=https://eclemma.svn.sourceforge.net/svnroot/eclemma/eclemmasite
+SVN_URL2=https://eclemma.svn.sourceforge.net/svnroot/eclemma/eclemma/tags/v1.4.3/com.mountainminds.eclemma.doc
+SVN_URL3=https://eclemma.svn.sourceforge.net/svnroot/eclemma/eclemma/trunk/com.mountainminds.eclemma.updatesite
 
 CHECKOUT_PATH1=$CHECKOUT_DIR/eclemmasite
 CHECKOUT_PATH2=$CHECKOUT_DIR/com.mountainminds.eclemma.doc
