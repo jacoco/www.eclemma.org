7a1eecf1b711fa3c8a760dc15f5ec066d72b4511
Marc R. Hoffmann <hoffmann@mountainminds.com>
2012-09-27 23:10:00 +0200

    Adjust script for git.

--- updatesite.sh
+++ updatesite.sh
@@ -4,19 +4,26 @@ WORKING_DIR="`pwd`/work"
 CHECKOUT_DIR=$WORKING_DIR/checkout
 RESULT_DIR=$WORKING_DIR/result
 
-SVN_URL1=https://eclemma.svn.sourceforge.net/svnroot/eclemma/eclemmasite
-SVN_URL2=https://eclemma.svn.sourceforge.net/svnroot/eclemma/eclemma/tags/v2.1.4/com.mountainminds.eclemma.doc
+GIT_URL1=https://github.com/jacoco/www.eclemma.org.git
+CHECKOUT_PATH1=$CHECKOUT_DIR/www.eclemma.org
+BRANCH1=master
 
-CHECKOUT_PATH1=$CHECKOUT_DIR/eclemmasite
-CHECKOUT_PATH2=$CHECKOUT_DIR/com.mountainminds.eclemma.doc
+GIT_URL2=https://github.com/jacoco/eclemma.git
+CHECKOUT_PATH2=$CHECKOUT_DIR/eclemma
+BRANCH2=v2.1.4
 
 TARGET=mtnminds,eclemma@web.sourceforge.net:/home/groups/e/ec/eclemma/htdocs/
 
 rm -fr $WORKING_DIR
 mkdir $WORKING_DIR
 
-svn export --force $SVN_URL1 $CHECKOUT_PATH1
-svn export --force $SVN_URL2 $CHECKOUT_PATH2
+git clone -- $GIT_URL1 $CHECKOUT_PATH1
+cd $CHECKOUT_PATH1
+git checkout $BRANCH1
+
+git clone -- $GIT_URL2 $CHECKOUT_PATH2
+cd $CHECKOUT_PATH2
+git checkout $BRANCH2
 
 
 cd $CHECKOUT_PATH1
