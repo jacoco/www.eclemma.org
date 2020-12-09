03ecfa733d4e08ec891ba8687933b5f361c40508
Evgeny Mandrikov <mandrikov@gmail.com>
2020-12-09 09:55:48 +0100

    EclEmma 3.1.4

--- render.sh
+++ render.sh
@@ -6,7 +6,7 @@ RESULT_DIR=$WORKING_DIR/result
 
 GIT_URL_ECLEMMA=https://github.com/eclipse/eclemma.git
 CHECKOUT_PATH_ECLEMMA=$CHECKOUT_DIR/eclemma
-BRANCH_ECLEMMA=v3.1.3
+BRANCH_ECLEMMA=v3.1.4
 
 rm -fr $WORKING_DIR
 mkdir $WORKING_DIR
